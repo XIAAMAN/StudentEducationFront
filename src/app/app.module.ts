5936b821aa5e47915b31b7e8ccfa522b2c7d5b3d
xiaaman <756585746@qq.com>
2019-07-14 01:42:46 +0800

    初步完成登录界面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './login/header/header.component';
+import { BodyComponent } from './login/body/body.component';
+import { FooterComponent } from './login/footer/footer.component';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material';
+// import {MatSidenavModule, MatToolbarModule, MatIconModule} from '@angular/materialic';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomeComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
   ],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
